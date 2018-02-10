d997269cacb952c69b3ca3078f2104aa759a1610
ryo-a <maki62rjtt@gmail.com>
2018-02-11 05:18:01 +0900

    add copy/extract feature

--- index.js
+++ index.js
@@ -4,22 +4,27 @@ const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const { ipcMain } = require('electron');
 
+const os = require('os');
 const path = require('path');
 const fs = require('fs-extra');
 const url = require('url');
 const unzip = require('unzip');
 const iconv = require('iconv-lite');
 
-
-console.log(app.getPath('desktop'));
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({ width: 450, height: 450, useContentSize: true, titleBarStyle: 'hidden' });
+  mainWindow = new BrowserWindow({
+    width: 350,
+    height: 350,
+    useContentSize: true,
+    titleBarStyle: 'hidden',
+    frame: false,
+    maximizable: false
+  });
+
   // hide menu bar
-  //mainWindow.setMenu(null);
-
-
+  mainWindow.setMenu(null);
 
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, '/view/index.html'),
@@ -28,7 +33,7 @@ function createWindow() {
   }));
 
   //open dev tools
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools();
 
   mainWindow.on('closed', function () {
     mainWindow = null
@@ -51,23 +56,35 @@ app.on('activate', function () {
 
 ipcMain.on('filename', (event, droppedFile) => {
   if (droppedFile.name != undefined && droppedFile.path != undefined && droppedFile.type != undefined) {
-    let fileNameDecoded = new Buffer(droppedFile.name.toString(), 'binary');
-    let filePathDecoded = new Buffer(droppedFile.path.toString(), 'binary');
-    console.log('name is :' + iconv.decode(droppedFile.name.toString(), 'CP932') +
-      '/path is :' + droppedFile.path.toString() +
-      '/filetype is :' + droppedFile.type);
+    let tmpdirCopyPath = os.tmpdir() + '/' + droppedFile.name;
+    let tmpdirExtractPath = os.tmpdir() + '/' + droppedFile.nameNoExtension + '_suitoriExtract';
+    let imageDirPath = app.getPath('desktop') + '/' + '[Image]' + droppedFile.nameNoExtension;
 
-    //ファイル名読み込む→コピーする→unzipする→
-    // Excelなら ファイル名\xl\media 内に画像がある
-    fs.copy(droppedFile.path, app.getPath('desktop') + '/' + droppedFile.name)
-      .then(() => console.log('complete'))
+    //copy Office File
+    fs.copy(droppedFile.path, tmpdirCopyPath)
+      .then(() =>
+        //unzip
+        fs.createReadStream(tmpdirCopyPath)
+          .pipe(unzip.Extract({ path: tmpdirExtractPath }))
+          .on('close', () => {
+            fs.unlink(tmpdirCopyPath); //delete Copied Office File
+
+            if (droppedFile.type == 'application/vnd.openxmlformats-officedocument.wordprocessingml.document') {
+              fs.copy(tmpdirExtractPath + '/word/media', imageDirPath);
+
+            } else if (droppedFile.type == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet') {
+              fs.copy(tmpdirExtractPath + '/xl/media', imageDirPath);
+
+            } else if (droppedFile.type == 'application/vnd.openxmlformats-officedocument.presentationml.presentation') {
+              fs.copy(tmpdirExtractPath + '/ppt/media', imageDirPath);
+
+            } else {
+              console.log('Unknown File');
+              return null;
+            }
+          })
+      )
       .catch(err => console.error(err))
-
-
   }
-
-
-
-
-  event.returnValue = 'pong';
+  //event.returnValue = '';
 })
