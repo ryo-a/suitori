f5fc159a548f19c4de80e2f633b610c5fe4b9a63
ryo-a <maki62rjtt@gmail.com>
2018-02-11 05:18:52 +0900

    add property (filename without extension) to object

--- renderer.js
+++ renderer.js
@@ -1,5 +1,7 @@
-const ipc = require('electron').ipcRenderer
+const path = require('path');
+const ipc = require('electron').ipcRenderer;
 const iconv = require('iconv-lite');
+
 
 
 /* D&D無効化 */
@@ -7,15 +9,21 @@ document.ondragover = document.ondrop = function (event) {
   event.preventDefault();
 }
 
-
 /* file list */
 document.body.addEventListener('drop', function (event) {
   console.log('file dropped:', event.dataTransfer.files);
-  //console.log('length:' + event.dataTransfer.files.length);
   for (let i in event.dataTransfer.files) {
-    let droppedOfficeFile = { name: event.dataTransfer.files[i].name, path: event.dataTransfer.files[i].path, type: event.dataTransfer.files[i].type};
+    let droppedFileNameWithoutExtension = event.dataTransfer.files[i].name.replace(path.extname(event.dataTransfer.files[i].name), "");
+
+    let droppedOfficeFile = {
+      name: event.dataTransfer.files[i].name,
+      nameNoExtension: droppedFileNameWithoutExtension,
+      path: event.dataTransfer.files[i].path,
+      type: event.dataTransfer.files[i].type
+    };
+    
+    /* example: name: hoge.docx, path: C:\Documents\hoge.docx,  type:application/vnd.openxmlformats-officedocument.wordprocessingml.document */
     ipc.sendSync('filename', droppedOfficeFile);
     console.log(droppedOfficeFile);
-    }
-  //
+  }
 });
